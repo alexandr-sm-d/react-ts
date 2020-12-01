72ab6166c36e9e8299516cff8e36ae94ed617791
Alexandr Smirnov <alexsm.dev@gmail.com>
2020-12-01 17:48:31 +0300

    input ref

--- src/components/ToDoForm.tsx
+++ src/components/ToDoForm.tsx
@@ -1,29 +1,20 @@
-import React, {useEffect, useState} from "react"
+import React, {useRef} from "react"
 
 export const ToDoForm: React.FunctionComponent = () => {
-    const [title, setTitle] = useState<string>('hi')
+    const ref = useRef<HTMLInputElement>(null)
 
-    const inputChangeHandler = (event: React.ChangeEvent<HTMLInputElement>): void => {
-        setTitle(event.target.value)
-    }
-
-    const keyPressHandler = (event: React.KeyboardEvent) : void => {
+    const keyPressHandler = (event: React.KeyboardEvent): void => {
         if (event.key === 'Enter') {
-            console.log(title)
+            console.log(ref.current!.value)
         }
     }
-
-    useEffect(() => {
-        console.log(title)
-    })
 
     return (
         <div className='input-filed mt2'>
             <label htmlFor="todo">Enter todo</label>
             <input
-                onChange={inputChangeHandler}
+                ref={ref}
                 onKeyPress={keyPressHandler}
-                value={title}
                 type="text"
                 placeholder='enter todo'
                 id='todo'
