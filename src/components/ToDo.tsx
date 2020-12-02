0e30bb90ec27eb52365965bfb288ca525872822b
Alexandr Smirnov <alexsm.dev@gmail.com>
2020-12-02 11:08:01 +0300

    cancel delete bug is fixed

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, {MouseEventHandler} from "react";
 import {IToDo} from "../interfaces";
 
 interface ToDoAction extends IToDo {
     onToggle(id: number): void,
+
     onRemove(id: number): void,
 }
 
@@ -12,12 +13,17 @@ export const ToDo: React.FunctionComponent<ToDoAction> = ({title, id, complited,
         classes.push('comleted')
     }
 
+    const onRemove = (event: React.MouseEvent, id: number) => {
+        event.preventDefault()
+        props.onRemove(id)
+    }
+
     return (
         <li className={classes.join(' ')}>
             <label>
                 <input type="checkbox" checked={complited} onChange={() => props.onToggle(id)}/>
                 <span>{title}</span>
-                <i className='material-icons red-text' onClick={props.onRemove.bind(null, id)}>delete</i>
+                <i className='material-icons red-text' onClick={event => onRemove(event, id)}>delete</i>
             </label>
         </li>
     )
